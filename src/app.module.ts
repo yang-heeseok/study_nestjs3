708f1d9a32f02b1f29c8468a204fb70520b00532
oggosil <airtown.yang@gmail.com>
2024-02-22 17:51:24 +0900

    [study] TypeORM setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './orm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({ useFactory: ormConfig }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
